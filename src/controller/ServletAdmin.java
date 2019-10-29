7cd4ae852c137cbc749d82e7f85d59ae2f0c34f4
CFerrara98 <carmine98.ferrara@gmail.com>
2019-10-29 16:23:36 +0100

    fix bug servlet_admin

--- src/controller/ServletAdmin.java
+++ src/controller/ServletAdmin.java
@@ -50,7 +50,7 @@ public class ServletAdmin<WritableWorkbook> extends HttpServlet {
 
     UserInterface user = (UserInterface) request.getSession().getAttribute("user");
 
-    if (flag == 5 || flag == 6) { // Genera Excel //bug - non può essere 5 e 6 contemporaneamente 
+    if (flag == 5 || flag == 6) { // Genera Excel //bug - non può essere 5 e 6 contemporaneamente 8
       PrintWriter out = response.getWriter();
       String content = "";
       Statement stmtSelect = null;
