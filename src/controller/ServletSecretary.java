c2b225cb2a6357c87392381948320166e3201bfc
black <black@black-N56JN>
2019-10-29 18:33:41 +0100

    fixed

--- src/controller/ServletSecretary.java
+++ src/controller/ServletSecretary.java
@@ -50,7 +50,8 @@ public class ServletSecretary extends HttpServlet {
    * response)
    */
   public void doPost(HttpServletRequest request, HttpServletResponse response)
-      throws ServletException, IOException {
+      throws ServletException, IOException 
+  {
     Integer result = 0; // indica se la query � riuscita
     String error = ""; // indica il mex di errore
     String content = ""; // indica il contenuto trovato dopo la query
@@ -64,12 +65,15 @@ public class ServletSecretary extends HttpServlet {
     Connection conn = new DbConnection().getInstance().getConn();
     String sql = "";
 
-    if (conn != null) {
+    if (conn != null) 
+    {
       Integer requestWorkingSecretary = Integer
           .parseInt(new SystemAttribute().getValueByKey("request-working-secretary"));
 
-      if (flag == 1) { // Preleva tutte le richieste
-        try {
+      if (flag == 1) 
+      { // Preleva tutte le richieste
+        try 
+        {
           stmtSelect = conn.createStatement();
           sql = "SELECT r.id_request, r.certificate_serial, r.level, r.release_date,"
               + " r.expiry_date, r.year, r.requested_cfu, r.serial, r.validated_cfu,"
@@ -80,23 +84,32 @@ public class ServletSecretary extends HttpServlet {
               + "     INNER JOIN user u ON r.fk_user = u.email " + "WHERE s.id_state IN("
               + requestWorkingSecretary
               + ")";
-          ResultSet r = stmtSelect.executeQuery(null);
-          if (r.wasNull()) {
+//          ResultSet r = stmtSelect.executeQuery(null);
+          ResultSet r = stmtSelect.executeQuery(sql);
+          if (r.wasNull()) 
+          {
             error = "Errore nell'esecuzione della Query";
-          } else {
+          }
+          else
+          {
             result = 1;
             int count = r.last() ? r.getRow() : 0;
-            if (count > 0) {
+            if (count > 0)
+            {
               r.beforeFirst();
               SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
               String classe = "even";
-              while (r.next()) {
-                if (classe.equals("odd")) {
+              while (r.next())
+              {
+                if (classe.equals("odd")) 
+                {
                   classe = "even";
-                } else {
+                }
+                else 
+                {
                   classe = "odd";
                 }
-
+ 
                 content += "<tr class='" + classe + "' role='row'>";
                 content += "    <td class='text-center'>" + r.getString("id_request") + "</td>";
                 //for attached 
@@ -107,23 +120,33 @@ public class ServletSecretary extends HttpServlet {
                     + "FROM attached a "
                     + "WHERE a.fk_request = " + idRequest + ";";
                 ResultSet r2 = stmtSelectTwo.executeQuery(sql);
-                if (r2.wasNull()) {
+                if (r2.wasNull())
+                {
                   result *= 0;
                   error = "Errore nell'esecuzione della Query degli Allegati";
-                } else {
+                }
+                else
+                {
                   int countAttached = r2.last() ? r2.getRow() : 0;
                   int i = 1;
-                  if (countAttached > 0) {
+                  if (countAttached > 0) 
+                  {
                     r2.beforeFirst();
-                    while (r2.next()) {
-                      if (i == countAttached) {
+                    while (r2.next()) 
+                    {
+                      if (i == countAttached)
+                      {
                         content += "<a href='" + request.getContextPath() + "/Downloader?filename=" + r2.getString("filename") + "&idRequest=" + idRequest + "'>" + r2.getString("filename") + "</a>";
-                      } else {
+                      }
+                      else 
+                      {
                         content += "<a href='" + request.getContextPath() + "/Downloader?filename=" + r2.getString("filename") + "&idRequest=" + idRequest + "'>" + r2.getString("filename") + "</a>" + " - ";
                       }                        
                       i++;
-                    }                      
+                    }
+                    
                   }
+                  
                 }
                 content += "    </td>";
                 content += "    <td class='text-center'>" + r.getString("serial") + "</td>";
@@ -164,8 +187,11 @@ public class ServletSecretary extends HttpServlet {
                     + "><i class=\"fa fa-check\"></i></button>";
                 
                 content += "</td>";
-              }              
-            } else {
+              }
+              
+            }
+            else
+            {
               content += "<tr>"
               		+ "<td class=\"text-center\"" + "></td>"
             		+ "<td class=\"text-center\"" + "></td>"
@@ -177,53 +203,82 @@ public class ServletSecretary extends HttpServlet {
               		+ "<td class=\"text-center\"" + "></td>"
               		+ "</tr>";
             }
+            
           }
-        } catch (Exception e) {
+          
+        }
+        catch (Exception e) 
+        {
           error += e.getMessage();
         }
 
-      } else if (flag == 2) { //Set cfu     
-    	  if(flag == 2)
-    		  throw new IllegalArgumentException("Parametro non valido");
-    	  
+      } 
+      else if (flag == 2) { //Set cfu     
+//    	  if(flag == 2)
+//    		  throw new IllegalArgumentException("Parametro non valido");
+    	
+    	 
         Integer idRequest = Integer.parseInt(request.getParameter("idRequest"));
         Integer cfu = Integer.parseInt(request.getParameter("cfu"));
         
-        try {
+        try
+        {
+        
+        	// da chiedere se vanno inseriti qui dentro per testare anche l'eccezione
+        	/*
+        	Integer idRequest = Integer.parseInt(request.getParameter("idRequest"));
+        	Integer cfu = Integer.parseInt(request.getParameter("cfu"));
+        	*/
+        	
           sql = " UPDATE request SET validated_cfu = ? WHERE id_request = ?; ";
           stmt = conn.prepareStatement(sql);
           stmt.setInt(1, cfu);
           stmt.setInt(2, idRequest);
-          if (stmt.executeUpdate() > 0) {
+          if (stmt.executeUpdate() > 0) 
+          {
             result = 1;
             content = "CFU aggiornati con successo.";
-          } else {
+          }
+          else 
+          {
             error += " Impossibile cambiare i CFU nella richiesta.";
             result = 0;
           }
 
-          if (result == 0) {
+          if (result == 0) 
+          {
             conn.rollback();
             result *= 0;
-          } else {
+          }
+          else 
+          {
             conn.commit();
           }
 
-        } catch (Exception e) {
+        } 
+        catch (Exception e) 
+        {
           error += e.getMessage();
           result = 0;
-        }        
-      } else if (flag == 3) { //Inoltra all'admin
+        }
+        
+      } 
+      else if (flag == 3) 
+      { //Inoltra all'admin
     	  
         Integer idRequest = Integer.parseInt(request.getParameter("idRequest"));
         Integer requestWorkingAdminState = Integer
             .parseInt(new SystemAttribute().getValueByKey("request-working-admin"));
         
-        try {
+        try 
+        {
           sql = " UPDATE request SET fk_state = ? WHERE id_request = ?; ";
           stmt = conn.prepareStatement(sql);
-          stmt.setInt(1, idRequest);
-          stmt.setInt(2, requestWorkingAdminState);
+//          stmt.setInt(1, idRequest);
+//          stmt.setInt(2, requestWorkingAdminState);
+          
+          stmt.setInt(1, requestWorkingAdminState);
+          stmt.setInt(2, idRequest);
           if (stmt.executeUpdate() > 0) {
             result = 1;
             content = "Richiesta inoltrata all'amministratore con successo.";
@@ -239,13 +294,18 @@ public class ServletSecretary extends HttpServlet {
             conn.commit();
           }
 
-        } catch (Exception e) {
+        } 
+        catch (Exception e) 
+        {
           error += e.getMessage();
           result = 0;
-        }        
+        }
+        
       }    
 
-    } else {
+    }
+    else
+    {
       result = 0;
       error = "Nessuna connesione al DB";
     }
@@ -258,4 +318,6 @@ public class ServletSecretary extends HttpServlet {
     out.println(res);
     response.setContentType("json");
   }
+  
+
 }
